56ea7dd0e46708f4102a0235066b02dafd5b04e0
Unknown <timofteca@gmail.com>
2018-04-19 21:04:57 +0300

    added test account

---
+++ oop/Laboratory7_8/Laboratory7_8/testAccount.cpp
@@ -0,0 +1,39 @@
+//
+//  testAccount.cpp
+//  Laboratory7_8
+//
+//  Created by Timofte Ciprian Andrei on 19/04/2018.
+//  Copyright © 2018 Timofte Ciprian Andrei. All rights reserved.
+//
+
+#include "testAccount.hpp"
+#include <iostream>
+#include "Account.hpp"
+#include <string>
+#include <assert.h>
+
+using namespace std;
+
+void testAccount() {
+    Account c1 = Account();
+    assert(c1.getPin() == 0);
+    assert(c1.getName() == "");
+    assert(c1.getIdCard() == "");
+    
+    string name="Dorel";
+    
+    Account c2(name,"19854563652",1234);
+    assert(c2.getName() == "Dorel");
+    assert(c2.getIdCard() == "19854563652");
+    assert(c2.getPin() == 1234);
+    
+    c2.setPin(1010);
+    assert(c2.getPin() == 1010);
+    
+    c2.setName("Andrei");
+    assert(c2.getName() == "Andrei");
+    
+    
+    cout << "tests passed successfully"<<endl;
+    
+}
---
+++ oop/Laboratory7_8/Laboratory7_8/testAccount.hpp
@@ -0,0 +1,16 @@
+//
+//  testAccount.hpp
+//  Laboratory7_8
+//
+//  Created by Timofte Ciprian Andrei on 19/04/2018.
+//  Copyright © 2018 Timofte Ciprian Andrei. All rights reserved.
+//
+
+#ifndef testAccount_hpp
+#define testAccount_hpp
+
+#include <stdio.h>
+
+void testAccount();
+
+#endif /* testAccount_hpp */
