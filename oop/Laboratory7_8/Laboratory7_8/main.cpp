010c31d21ade617457b92f0b46bc382d15500e42
Unknown <timofteca@gmail.com>
2018-04-19 21:16:51 +0300

    added main

---
+++ oop/Laboratory7_8/Laboratory7_8/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Laboratory7_8
+//
+//  Created by Timofte Ciprian Andrei on 19/04/2018.
+//  Copyright © 2018 Timofte Ciprian Andrei. All rights reserved.
+//
+
+#include <iostream>
+#include "testAccount.hpp"
+#include "testRepository.hpp"
+
+
+int main(int argc, const char * argv[]) {
+    testAccount();
+    testRepository();
+    return 0;
+}
