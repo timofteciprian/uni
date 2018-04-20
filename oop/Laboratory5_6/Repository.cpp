6a681cae91b4a51d5e8aea5981dfb7d0fd43e747
Unknown <timofteca@gmail.com>
2018-04-20 16:56:13 +0300

    added description

--- oop/Laboratory5_6/Repository.cpp
+++ oop/Laboratory5_6/Repository.cpp
@@ -9,7 +9,7 @@
 #include "Repository.hpp"
 #include <iostream>
 /*
- decr: adauga un obiect de tip Expence
+ decr: adds an Expence object
  in: ex
  out:-
  */
@@ -17,42 +17,70 @@ void Repository::addExpense(Expense ex){
     this->costs = this->costs + ex;
 }
 /*
- decr: returneaza lista de tip Expence
+ decr: returns the Expense list
  in:
  out: costs
  */
 DynamicVector<Expense> &Repository::getAll(){
     return this->costs;
 }
-
+/*
+ decr: Eliminate cost of apartment
+ in: expense
+ out:
+ */
 void Repository::elimCostsOfApartment(Expense ex){
     this->costs = this->costs - ex;
 }
-
+/*
+ decr: Replace sum for a expense
+ in: index(expense), new sum
+ out:
+ */
 void Repository::replaceSumExpense(int index, unsigned int newSum){
     this->costs[index].setSum(newSum);
 }
-
+/*
+ decr: delete all explense
+ in:
+ out:
+ */
 void Repository::clearAll(){
     this->costs.clear();
 }
-
+/*
+ decr: Change two expenses between them
+ in:
+ out:
+ */
 void Repository::changePosition(int a, int b){
     swap(this->costs[a],this->costs[b]);
 }
-
+/*
+ decr: Operator addition
+ in:
+ out:
+ */
 DynamicVector <Expense> operator + (const DynamicVector <Expense> &a, const Expense &b) {
     DynamicVector <Expense> ret = a;
     ret.push_back(b);
     return ret;
 }
-
+/*
+ decr: Operator addition
+ in:
+ out:
+ */
 DynamicVector <Expense> operator + (const Expense &b, const DynamicVector <Expense> &a) {
     DynamicVector <Expense> ret = a;
     ret.push_back(b);
     return ret;
 }
-
+/*
+ decr: Operator minus
+ in:
+ out:
+ */
 DynamicVector <Expense> operator - (const DynamicVector <Expense> &a, const Expense &b) {
     DynamicVector <Expense> ret = a;
     ret.remove(b);
