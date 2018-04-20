e416378c8bdd151e5189defdc3364df41e62c9f7
Unknown <timofteca@gmail.com>
2018-04-20 16:57:26 +0300

    added tests

--- oop/Laboratory5_6/main.cpp
+++ oop/Laboratory5_6/main.cpp
@@ -10,11 +10,17 @@
 #include "Repository.hpp"
 #include "Controller.hpp"
 #include "Ui.hpp"
+#include "testsExpense.hpp"
+#include "testsController.hpp"
+#include "testsRepository.hpp"
 
 using namespace std;
 
 int main(int argc, const char * argv[]) {
     
+    testExpense();
+    testRepo();
+    testController();
     
     Repository repo;
     Controller ctrl(repo);
