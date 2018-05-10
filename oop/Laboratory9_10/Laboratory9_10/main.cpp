8ed46e4cd9f4f4bca9029eb6e0c79977bed427e7
Unknown <timofteca@gmail.com>
2018-05-10 22:07:55 +0300

    added main

---
+++ oop/Laboratory9_10/Laboratory9_10/main.cpp
@@ -0,0 +1,24 @@
+//
+//  main.cpp
+//  Lab9_10
+//
+//  Created by Timofte Ciprian Andrei on 08/05/2018.
+//  Copyright © 2018 Timofte Ciprian Andrei. All rights reserved.
+//
+
+#include "Repository.hpp"
+#include "RepositoryUser.hpp"
+#include "Ui.hpp"
+#include <iostream>
+#include <fstream>
+
+
+int main(int argc, const char * argv[]) {
+    Repository repoM("fileMaterial.txt");
+    Repository  repoF("fileFinancial.txt");
+    RepositoryUser repoUser("users.txt");
+    Controller ctrl={repoM, repoF, repoUser};
+    Ui ui{ctrl};
+    ui.run();
+    return 0;
+};
