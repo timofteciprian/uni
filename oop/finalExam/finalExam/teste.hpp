990d7c0e83a388df4941e622297fcbd14e6add46
Unknown <timofteca@gmail.com>
2018-06-08 23:52:21 +0300

    added tests

---
+++ oop/finalExam/finalExam/teste.hpp
@@ -0,0 +1,102 @@
+//
+//  teste.hpp
+//  finalExam
+//
+//  Created by Timofte Ciprian Andrei on 08/06/2018.
+//  Copyright © 2018 Timofte Ciprian Andrei. All rights reserved.
+//
+
+#ifndef teste_hpp
+#define teste_hpp
+
+#include <stdio.h>
+#include "Repo.hpp"
+#include "RepoFile.hpp"
+#include "Cusca.hpp"
+
+void testCusca();
+
+#endif /* teste_hpp */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
